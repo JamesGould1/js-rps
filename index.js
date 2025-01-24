47a19193c5e2474f7dbb99077ce2e75867e7e19f
James Goulding <james.goulding@ten10.com>
2025-01-24 11:13:51 +0000

    Added in rudimentary moves array with const of moves containing toString method and beats method

---
+++ index.js
@@ -0,0 +1,14 @@
+const ROCK = {toString: () => "Rock", 
+    beats: (other) => other === SCISSORS}
+const PAPER = {toString: () => "Paper", 
+    beats: (other) => other === ROCK}
+const SCISSORS = {toString: () => "Scissors", 
+    beats: (other) => {if (other === SCISSORS) {return "draw"} else other === PAPER}}
+
+const MOVES = [ROCK,PAPER,SCISSORS]
+
+for (const aggressor of MOVES) {
+    for(const defender of MOVES) {
+    console.log(`Does ${aggressor} beat ${defender}? ${aggressor.beats(defender) ? 'yes' : 'no'}`)    
+    }
+}
