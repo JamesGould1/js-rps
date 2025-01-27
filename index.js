3f30043bd80f3b67ace47d41f68b3a669546cb56
James Goulding <james.goulding@ten10.com>
2025-01-27 10:51:17 +0000

    Added more tests to play game function as well as replay on draw

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
-const ROCK = {toString: () => "Rock", 
-    beats: (other) => other === SCISSORS}
-const PAPER = {toString: () => "Paper", 
-    beats: (other) => other === ROCK}
-const SCISSORS = {toString: () => "Scissors", 
-    beats: (other) =>  other === PAPER}
+const ROCK = {
+    toString: () => "Rock",
+    beats: (other) => other === SCISSORS
+}
+const PAPER = {
+    toString: () => "Paper",
+    beats: (other) => other === ROCK
+}
+const SCISSORS = {
+    toString: () => "Scissors",
+    beats: (other) => other === PAPER
+}
 
-const MOVES = [ROCK,PAPER,SCISSORS]
+const MOVES = [ROCK, PAPER, SCISSORS]
 const computerplayer = {
-    chooseMove: () => {return MOVES[Math.floor(Math.random() * MOVES.length)]}
+    chooseMove: () => { return MOVES[Math.floor(Math.random() * MOVES.length)] }
 }
 const humanPlayer = {
     playerSelectedValue: 1,
-    chooseMove: () => {return MOVES[playerSelectedValue-1]}
+    chooseMove: () => { return MOVES[playerSelectedValue - 1] }
 }
-const playGame = (player1, player2) => {
-    let move1 = player1.chooseMove();
-    let move2 = player2.chooseMove();
-    let winner;
-
-    console.log("Player one played " +move1)
-    console.log("Player two played " +move2)
-    if (move1.beats(move2)) {
-        winner = player1
-        console.log("Player one won")
-    }
-    else if (move2.beats(move1)){
-        winner = player2
-        console.log("Player two won")
-    }
-    else {
-        console.log("It's a draw!")
-    }
-    return winner
+function playGame(player1, player2) {
+    let draw = true;
+    while (draw) {
+        const move1 = player1.chooseMove()
+        const move2 = player2.chooseMove()
+        let winner
+        console.log(`${player1} played ${move1}`)
+        console.log(`${player2} played ${move2}`)
+        if (move1.beats(move2)) {
+            winner = player1
+            draw = false
+        }
+        else if (move2.beats(move1)) {
+            winner = player2
+            draw = false
+        }
+        else {
+            console.log("It's a draw!")
+        }
+    } console.log(`${winner} won`)
 }
-module.exports = {MOVES, computerplayer, humanPlayer, playGame}
+module.exports = { MOVES, computerplayer, humanPlayer, playGame }
--- index.test.js
+++ index.test.js
@@ -6,64 +6,89 @@ const SCISSORS = MOVES[2]
 
 
 describe(`MOVES`, () => {
-for (const move of MOVES) {
-    test(`${move} should beat one other move`, () => {
-        let victories = 0;
-        for (const defender of MOVES) {
-            victories += move.beats(defender)
-        }
-        expect(victories === 1).toBe(true)
-    })
-    test(`${move} should not beat itself`, () => {
-        expect(move.beats(move)).toBeFalsy
-    })
-    test(`${move} should lose to one other move`, () => {
-        let defeats = 0;
-        for (const aggressor of MOVES) {
-            defeats += aggressor.beats(move)
-        }
-        expect(defeats === 1).toBe(true)
+    for (const move of MOVES) {
+        test(`${move} should beat one other move`, () => {
+            let victories = 0;
+            for (const defender of MOVES) {
+                victories += move.beats(defender)
+            }
+            expect(victories === 1).toBe(true)
+        })
+        test(`${move} should not beat itself`, () => {
+            expect(move.beats(move)).toBeFalsy
+        })
+        test(`${move} should lose to one other move`, () => {
+            let defeats = 0;
+            for (const aggressor of MOVES) {
+                defeats += aggressor.beats(move)
+            }
+            expect(defeats === 1).toBe(true)
 
-    })
-}})
+        })
+    }
+})
 
 describe('computerPlayer', () => {
     const realRandom = Math.random;
-    const cases = [[ROCK, 0.1],[PAPER, 0.4],[SCISSORS, 0.7]]
+    const cases = [[ROCK, 0.1], [PAPER, 0.4], [SCISSORS, 0.7]]
     afterAll(() => {
-      Math.random = realRandom;
+        Math.random = realRandom;
     });
-  
+
     for (const [move, randomValue] of cases) {
-      test(`${move} should be returned when Math.random returns ${randomValue}`, () => {
-        Math.random = () => {return randomValue};
-        const chosenMove = computerplayer.chooseMove();
-        expect(chosenMove).toBe(move);
-      });
+        test(`${move} should be returned when Math.random returns ${randomValue}`, () => {
+            Math.random = () => { return randomValue };
+            const chosenMove = computerplayer.chooseMove();
+            expect(chosenMove).toBe(move);
+        });
     }
 });
 
 describe('humanPlayer', () => {
-    const cases = [[ROCK,1],[PAPER,2],[SCISSORS, 3]]
-    for (const [move, value] of cases){
-    test(`Human Player should select ${move} when they provide ${value}`, () => {
-        playerSelectedValue = value;
-        const chosenMove = humanPlayer.chooseMove();
-        expect(chosenMove).toBe(move)
-    }) 
-}})
+    const cases = [[ROCK, 1], [PAPER, 2], [SCISSORS, 3]]
+    for (const [move, value] of cases) {
+        test(`Human Player should select ${move} when they provide ${value}`, () => {
+            playerSelectedValue = value;
+            const chosenMove = humanPlayer.chooseMove();
+            expect(chosenMove).toBe(move)
+        })
+    }
+})
 
 describe('playGame', () => {
-    const playsRock = {toString: ()=> "Rocky", chooseMove: () => {return ROCK}}
-    const playsPaper = {toString: ()=> "Papery", chooseMove: () => {return PAPER}}
-    const consoleLog = console.log
-    let consoleMessage = consoleLog
+    const playsRock = { toString: () => "Rocky", chooseMove: () => { return ROCK } }
+    const playsPaper = { toString: () => "Papery", chooseMove: () => { return PAPER } }
+    const playsRockThenPaper = { toString: () => "SoftRock", chooseMove: jest.fn().mockReturnValueOnce(ROCK).mockReturnValueOnce(PAPER)}
+    const realConsoleLog = console.log
+
+    beforeEach(() => {
+        console.log = jest.fn()
+    })
+
+    afterEach(() => {
+        console.log = realConsoleLog
+    })
 
 
-
-    test(`player 1 move should be printed`, () => { 
-        let winner;
-        winner = playGame(playsRock, playsPaper) 
-        expect(consoleMessage.toString).toBe("Player one played Rock")
+    test(`player 1 move should be printed`, () => {
+        playGame(playsRock, playsPaper)
+        expect(console.log).toHaveBeenCalledWith('Rocky played Rock')
+    })
+    test(`player 2 move should be printed`, () => {
+        playGame(playsPaper, playsRock)
+        expect(console.log).toHaveBeenCalledWith('Papery played Paper')
+    })
+    test(`player1's wins should be reported`, () => {
+        playGame(playsRock, playsPaper)
+        expect(console.log).toHaveBeenCalledWith('Papery won')
+    })
+    test(`player2's wins should be reported`, () => {
+        playGame(playsPaper, playsRock)
+        expect(console.log).toHaveBeenCalledWith('Papery won')
+    })
+    test(`draws should be reported and cause a replay`, () => {
+        playGame(playsRockThenPaper, playsRock)
+        expect(console.log).toHaveBeenCalledWith("It's a draw!")
+        expect(console.log).toHaveBeenCalledWith("SoftRock won")
     })
 })
